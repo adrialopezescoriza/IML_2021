aa9d042b0c06c41a13820b057e030dd989a11769
Adria Lopez Escoriza <adrialopezescoriza@student-net-als-0703.ethz.ch>
2021-03-03 17:58:36 +0100

    Project 0 solved

--- Project_0/main.py
+++ Project_0/main.py
@@ -17,5 +17,4 @@ results = np.vstack((test_id,test_predictions)).T
 
 ## Print test outputs to csv files
 header = np.array(["Id, y\n"])
-#np.savetxt('results.csv', header, fmt="%s", delimiter=',')
 np.savetxt('results.csv', results, fmt=['%.0f','%.1f'], delimiter=',', header="Id, y", comments='')
