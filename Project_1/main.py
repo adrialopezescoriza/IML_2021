92dce54212674c66d5d68826e3860a511168504f
Adria Lopez Escoriza <adrialopezescoriza@student-net-als-0467.ethz.ch>
2021-03-09 15:30:00 +0100

    First solution

--- Project_1/main.py
+++ Project_1/main.py
@@ -1,19 +1,51 @@
 from sklearn.linear_model import Ridge
 import numpy as np
 
+def split_dataset(training_data):
+    length = np.shape(training_data)[0]
+    split_set = np.split(training_data,10,axis=0)
+    return split_set
+
 ########################
 ### Main for task 1a ###
 ########################
 
 # Regularisation parameter
 lambda_reg = [0.1,1,10,100,200]
+lambda_score = np.zeros(len(lambda_reg))
 
 ## Load Data from csv files
-train_data = np.genfromtxt('train.csv', delimiter=',')[1:,:]
+train_data = np.genfromtxt('Project_1/train.csv', delimiter=',')[1:,:]
+data = split_dataset(train_data)
+
+## Validate model for every alpha
+j = 0
+for alpha in lambda_reg:
+    ## Get validation 10 times
+    ridge_regressor = Ridge(alpha)
+    score = np.zeros((1,len(data)))
+    i = 0
+    for cv_set in data:
+        ## Train with remaining data
+        aux_data = data[:]
+        train_set = np.array(aux_data.pop(i))
+        ridge_regressor.fit(train_set[:,1:],train_set[:,0:1])
+
+        ## Evaluation
+        score[0,i] = ridge_regressor.score(cv_set[:,1:],cv_set[:,0:1])
+        i += 1
+    
+    # Compute average score
+    avg_score = np.mean(score)
+
+    # Append to lambda_score
+    lambda_score[j] = avg_score
+    j += 1
+
+## Print test outputs to csv files
+np.savetxt('Project_1/scores.csv', lambda_score, fmt=['%.3f'], delimiter='\n', comments='')
 
 
-def split_dataset(training_data):
-    length = np.shape(training_data)[0]
-    split_set = np.split(training_data,10,axis=0)
+
 
 
