c16bde18d0234d3001a53d03ed107e4fbb234a7f
Adria Lopez Escoriza <adrialopezescoriza@student-net-hg-2142.ethz.ch>
2021-05-18 15:53:05 +0200

    Code running like shit

--- Project_4/main.py
+++ Project_4/main.py
@@ -24,24 +24,24 @@ import matplotlib.pyplot as plt
 
 ################ Config params ##################
 # Network parameters
-l1_out = 40
-l2_out = 60
-l3_out = 80
-l3_out = 100
-l4_out = 120
-l5_out = 140
-l6_out = 120
-l7_out = 100
-l8_out = 80
-l9_out = 60
-l10_out = 40
+l1_out = 500
+l2_out = 600
+l3_out = 700
+l3_out = 800
+l4_out = 700
+l5_out = 600
+l6_out = 500
+l7_out = 400
+l8_out = 200
+l9_out = 50
+l10_out = 5
 n_epochs = 500
-lr = 5e-4
-wd = 2e-3
+lr = 1e-3
+wd = 0
 
 # Training params
 train_frac = 0.9
-b_size = 30
+b_size = 50
 only_test = False
 
 ################ Functions ##################
@@ -69,12 +69,12 @@ class Net(nn.Module):
         self.relu = nn.ReLU()
         self.fc2 = nn.Linear(l1_out, l2_out, bias=True)
         self.fc3 = nn.Linear(l2_out, l3_out, bias=True)
-        #self.fc4 = nn.Linear(l3_out, l4_out, bias=True)
-        #self.fc5 = nn.Linear(l4_out, l5_out, bias=True)
-        #self.fc6 = nn.Linear(l5_out, l6_out, bias=True)
-        #self.fc7 = nn.Linear(l6_out, l7_out, bias=True)
-        #self.fc8 = nn.Linear(l7_out, l8_out, bias=True)
-        self.fc9 = nn.Linear(l3_out, l9_out, bias=True)
+        self.fc4 = nn.Linear(l3_out, l4_out, bias=True)
+        self.fc5 = nn.Linear(l4_out, l5_out, bias=True)
+        self.fc6 = nn.Linear(l5_out, l6_out, bias=True)
+        self.fc7 = nn.Linear(l6_out, l7_out, bias=True)
+        self.fc8 = nn.Linear(l7_out, l8_out, bias=True)
+        self.fc9 = nn.Linear(l8_out, l9_out, bias=True)
         self.fc10 = nn.Linear(l9_out, l10_out, bias=True)
         self.fc11 = nn.Linear(l10_out, size_output, bias=True)
         self.sig= nn.Sigmoid()
@@ -83,11 +83,11 @@ class Net(nn.Module):
         x = self.relu(self.fc1(x))
         x = self.relu(self.fc2(x))
         x = self.relu(self.fc3(x))
-        #x = self.relu(self.fc4(x))
-        #x = self.relu(self.fc5(x))
-        #x = self.relu(self.fc6(x))
-        #x = self.relu(self.fc7(x))
-        #x = self.relu(self.fc8(x))
+        x = self.relu(self.fc4(x))
+        x = self.relu(self.fc5(x))
+        x = self.relu(self.fc6(x))
+        x = self.relu(self.fc7(x))
+        x = self.relu(self.fc8(x))
         x = self.relu(self.fc9(x))
         x = self.relu(self.fc10(x))
         x = self.sig(self.fc11(x))
@@ -105,7 +105,8 @@ class Classifier():
         self.plot_loss = plot_loss
     
     def train(self, X, Y, X_cv, Y_cv, batch_size=b_size):
-        best_score_cv = 0
+        best_score_cv = 1
+        best_epoch = 0
         epoch_losses=[]
         for epoch in range(n_epochs):  # loop over the dataset multiple times
 
@@ -142,12 +143,13 @@ class Classifier():
                 BCE_loss_cv_dB    = 10*np.log10(loss_cv.item())
 
                 if (loss_cv.item() < best_score_cv):
-                    torch.save(self,'Project_3/best-model.pt')
+                    torch.save(self,'Project_4/best-model.pt')
                     best_score_cv = loss_cv.item()
+                    best_epoch = epoch
 
                 print('\nEpoch',epoch,'training loss: ', np.mean(losses))#BCE_loss_train_dB,'[dB]')
                 print('Epoch',epoch,'validation loss: ', loss_cv.item())#BCE_loss_cv_dB,'[dB]') 
-                print('Best score: ',best_score_cv)
+                print('Best loss: ',best_score_cv, '; epoch:', best_epoch)
 
         if self.plot_loss:
             plt.plot(epoch_losses)
@@ -163,24 +165,23 @@ class Classifier():
 ################ Code ##################
 if (not only_test):
     # Load datasets
-    df = pd.read_csv('Project_3/train.csv')
+    dataset = torch.from_numpy(np.genfromtxt('Project_4/train.csv',delimiter=',').astype('int'))
 
     # Split into validation and training set
-    df_size = df.size/2
-    df_train = df.iloc[:round(df_size*train_frac),:]
-    df_cv    = df.iloc[round(df_size*train_frac):,:]
+    df_size = dataset.size(0)
+    df_train = dataset[0:int(df_size*(1-train_frac)),:]
+    df_val = dataset[0:int(df_size*(train_frac)),:]
 
-    X_train = df_train['Food_class']
-    X_val = df_cv['Food_class']
+    X_train = df_train[:,0:303]
+    X_val = df_val[:,0:303]
 
-    Y_train = df_train['Correctness']
-    Y_val = df_cv['Correctness']
+    Y_train = df_train[:,303]
+    Y_val = df_val[:,303]
 
+    print("Dataset loaded...")
     # Classifier model and training
     protein_Net = Classifier(303,1,plot_loss=False)
-    X,y = torch.from_numpy(X_train).float(), torch.from_numpy(Y_train).float()
-    X_cv,y_cv = torch.from_numpy(X_val).float(), torch.from_numpy(Y_val).float() 
-    protein_Net.train(X,y,X_cv,y_cv)
+    protein_Net.train(X_train.float(),Y_train.float(),X_val.float(),Y_val.float())
 
 else:
     # Save results
